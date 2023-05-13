6fe85a5d277a127ec65fabdc024c56010072de03
Taimoor Khan <82448357+taimooorr@users.noreply.github.com>
2023-05-13 14:33:38 +0500

    added navigation

---
+++ src/routes/Navigation/Navigation.jsx
@@ -0,0 +1,26 @@
+import { Link, Outlet } from "react-router-dom"
+import "./NavigationStyles.scss";
+import { ReactComponent as MainLogo } from "../../assests/crown.svg";
+export default function Navigation() {
+  return (
+		<>
+			<div className="navigation">
+				<Link className="logo-container" to="/">
+					<MainLogo className="logo" />
+				</Link>
+				<div className="nav-links-container">
+					<Link className="nav-link" to="/shop">
+						Shop
+					</Link>
+					<Link className="nav-link" to="/shop">
+						Contact
+          </Link>
+          <Link className="nav-link" to="/sign-in">
+            Sign In
+          </Link>
+				</div>
+			</div>
+			<Outlet />
+		</>
+  );
+}
