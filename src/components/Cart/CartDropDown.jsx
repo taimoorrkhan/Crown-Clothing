775b90469b8be3ad730dda0be0b7273f92bb68c2
Taimoor Khan <82448357+taimooorr@users.noreply.github.com>
2023-06-04 11:39:26 +0500

    added CartDropDown

--- src/components/Cart/CartDropDown.jsx
+++ src/components/Cart/CartDropDown.jsx
@@ -1,12 +1,27 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { useNavigate } from 'react-router-dom'
 import './CartDropDownStyles.scss'
 import MyButton from '../Button/MyButton'
+import CartItem from '../CartItem/CartItem'
+import { CartContext } from '../../Contexts/CartContext'
 export default function CartDropDown() {
+	const { cartItems } = useContext(CartContext);
+	const navigate = useNavigate();
+
+	const handleCheckout = () => {
+		navigate('/checkout');
+	};
   return (
-    <div className='cart-dropdown-container'>
-      <div className="cart-items">
-        <MyButton>Checkout</MyButton>
-      </div>
-    </div>
-  )
+		<div className="cart-dropdown-container">
+			<div className="cart-items">
+			
+				{cartItems &&
+					cartItems.map((item) => (
+						<CartItem CartItem={item} key={item.id} />
+					))}
+		
+			</div>
+			<MyButton onClick={handleCheckout} >Checkout</MyButton>
+		</div>
+  );
 }
