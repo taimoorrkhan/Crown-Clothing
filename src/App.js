ecb67347a420794c63057dea8307f0905bb0d42d
Taimoor Khan <82448357+taimooorr@users.noreply.github.com>
2023-06-04 11:43:13 +0500

    Fixed Render issues

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Home from './routes/home/Home';
 import Navigation from './routes/Navigation/Navigation';
 import Authentication from './components/authentication/Authentication';
 import Shops from './routes/Shop/Shops';
-
+import Checkout from './components/Checkout/Checkout';
 
 import { Route, Routes } from 'react-router-dom';
 
@@ -14,9 +14,9 @@ const App = () => {
     <Routes>
       <Route path='/' element={<Navigation />} >
         <Route index element={<Home />} />
-        <Route path='shop' element={<Shops />} />
+        <Route path='shop/*' element={<Shops />} />
         <Route path='auth' element={<Authentication />} />
-
+        <Route path='checkout' element={<Checkout />} />
       </Route>
     </Routes>
   );
--- src/index.js
+++ src/index.js
@@ -6,20 +6,19 @@ import { CartProvider } from './Contexts/CartContext';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 import { UserContextProvider } from './Contexts/UserContext';
-import { ProductsContextProvider } from './Contexts/ProductsContexts';
+import { CategoriesContextProvider } from './Contexts/CategoriesContext';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 
 
   <React.StrictMode>
     <BrowserRouter>
-      
       <UserContextProvider>
-        <ProductsContextProvider>
+        <CategoriesContextProvider>
           <CartProvider>
             <App />
           </CartProvider>
-        </ProductsContextProvider>
+        </CategoriesContextProvider>
       </UserContextProvider>
     </BrowserRouter>
   </React.StrictMode>
