52986988b92d410f80122e43ce34d0b9460918e7
Taimoor Khan <82448357+taimooorr@users.noreply.github.com>
2023-06-04 11:40:54 +0500

    added cart context

--- src/Contexts/CartContext.jsx
+++ src/Contexts/CartContext.jsx
@@ -1,19 +1,88 @@
-import { createContext, useState } from "react";
+import { createContext, useState,useEffect } from "react";
+
+const addCartItem = (cartItem, item) => {
+  const existingCartItem = cartItem.find(
+    (cartItem) => cartItem.id === item.id
+  );
+  if (existingCartItem) {
+    return cartItem.map((cartItem) =>
+      cartItem.id === item.id
+        ? { ...cartItem, quantity: cartItem.quantity + 1 }
+        : cartItem
+    );
+  }
+  return [...cartItem,{...item,quantity:1}]
+};
+const removeCartItem = (cartItem, itemToRemove) => {
+  const existingCartItem = cartItem.find(
+    (cartItem) => cartItem.id === itemToRemove.id
+  );
+  if (existingCartItem.quantity === 1) {
+    return cartItem.filter((cartItem) => cartItem.id !== itemToRemove.id);
+  }
+  return cartItem.map((cartItem) =>
+    cartItem.id === itemToRemove.id
+      ? { ...cartItem, quantity: cartItem.quantity - 1 }
+      : cartItem
+  );
+};
+const clearCartItem = (cartItem, itemToClear) => {
+  return cartItem.filter((cartItem) => cartItem.id !== itemToClear.id);
+
+};
+
 
 export const CartContext = createContext({
   isCartOpen: false,
   toggleCart: () => { },
+  cartItems: [],
+  addItemToCart: () => { },
+  removeItemFromCart: () => { },
+  clearItemFromCart: () => { },
+  total : 0
 });
 
 export const CartProvider = ({ children }) => {
   const [isCartOpen, setIsCartOpen] = useState(false);
+  const [cartItems, setCartItem] = useState([]);
+ const [cartCount, setCartCount] = useState(0);
+ const [cartTotal, setCartTotal] = useState(0);
 
+ useEffect(() => {
+		const newCartCount = cartItems.reduce(
+			(total, cartItem) => total + cartItem.quantity,
+			0
+		);
+		setCartCount(newCartCount);
+ }, [cartItems]);
+
+ useEffect(() => {
+		const newCartTotal = cartItems.reduce(
+			(total, cartItem) =>
+				total + cartItem.quantity * cartItem.price,
+			0
+		);
+		setCartTotal(newCartTotal);
+ }, [cartItems]);
+
+const addItemToCart = (item) => {
+    setCartItem(addCartItem(cartItems,item));
+  };
+
+  const removeItemFromCart = (item) => {
+    setCartItem(removeCartItem(cartItems,item));
+  };
+
+  const clearItemFromCart = (item) => {
+    setCartItem(clearCartItem(cartItems,item));
+    
+  }
   const toggleCart = () => {
     setIsCartOpen(!isCartOpen);
   };
 
   return (
-    <CartContext.Provider value={{ isCartOpen, toggleCart }}>
+    <CartContext.Provider value={{ isCartOpen, toggleCart,addItemToCart,cartItems ,removeItemFromCart ,clearItemFromCart,cartCount,cartTotal}}>
       {children}
     </CartContext.Provider>
   );
--- src/components/CategoryItem/CategoryItem.jsx
+++ src/components/CategoryItem/CategoryItem.jsx
@@ -1,16 +1,24 @@
 
 import './CategoryItem.scss'
+
+import { useNavigate } from 'react-router-dom';
+
 const CategoryItem = ({ category }) => {
-	const { imageUrl, title } = category;
+	const navigate = useNavigate();
+	const { imageUrl, title,route } = category;
+
+	const onNavigate = () => {
+		navigate(route);
+	};
 	return (
-		<div className="category-container">
+		<div onClick={onNavigate} className="category-item-container">
 			<div
 				className="background-image"
 				style={{
 					backgroundImage: `url(${imageUrl})`,
 				}}
 			/>
-			<div className="category-body-container">
+			<div className="category-item-body-container">
 				<h2>{title}</h2>
 				<p>Shop Now</p>
 			</div>
